402995e1230398bbe770341324d2d090b3dec01a
maximizzar <maximizzar@web.de>
2022-12-23 13:20:49 +0100

    init commit

---
+++ src/redBlackTree.java
@@ -0,0 +1,11 @@
+public class redBlackTree {
+
+    public String toString() {
+        //output function as Simple String.
+        return "";
+    }
+    public String toDOT() {
+        //output function in DOT format.
+        return "";
+    }
+}
