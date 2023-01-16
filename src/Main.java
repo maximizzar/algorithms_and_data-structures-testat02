58f6475995cff778779b1c9ce81a1777cac21a23
maximizzar <maximizzar@web.de>
2023-01-16 18:31:22 +0100

    scheiß auf import

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,6 @@
-import java.util.Random;
-
 public class Main {
     public static void main(String[] args) {
-        Random random = new Random();
+        java.util.Random random = new java.util.Random();
         RedBlackTree redBlackTree = new RedBlackTree(randomNumber(random));
         RedBlackTree gredBlackTree = new RedBlackTree(randomNumber(random));
         redBlackTree.setColor(false);
@@ -11,7 +9,7 @@ public class Main {
 
         System.out.println(redBlackTree.getValue());
     }
-    public static int randomNumber(Random random) {
+    public static int randomNumber(java.util.Random random) {
 
         return random.nextInt(99) + 1;
     }
