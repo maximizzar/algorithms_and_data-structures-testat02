6614396ad1142d851f6bf4ab63c545481bc45157
maximizzar <maximizzar@web.de>
2023-01-16 18:29:29 +0100

    rotateLeft + rotateRight + replaceParentsChild

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,18 @@
+import java.util.Random;
+
 public class Main {
     public static void main(String[] args) {
-        RedBlackTree redBlackTree = new RedBlackTree();
+        Random random = new Random();
+        RedBlackTree redBlackTree = new RedBlackTree(randomNumber(random));
+        RedBlackTree gredBlackTree = new RedBlackTree(randomNumber(random));
         redBlackTree.setColor(false);
         System.out.println("Hi Mom!");
         System.out.println(redBlackTree.getColorToString());
+
+        System.out.println(redBlackTree.getValue());
+    }
+    public static int randomNumber(Random random) {
+
+        return random.nextInt(99) + 1;
     }
 }
--- src/RedBlackTree.java
+++ src/RedBlackTree.java
@@ -2,23 +2,56 @@
  * a red-black tree implementation by the unavailable team!
  */
 public class RedBlackTree {
+    private RedBlackTree parent, childLeft, childRight;
+    RedBlackTree root;
+    private Integer value;
     private boolean color;
+
+    public RedBlackTree(Integer value) {
+        setValue(value);
+    }
+
     //rotations on red-black tree
-    public void rotateLeft(RedBlackTree node) {
+    public void rotateLeft(RedBlackTree rbtNode) {
+        RedBlackTree parent = rbtNode.getParent();
+        RedBlackTree rightChild = rbtNode.childRight.getChildRight();
 
-    }
-    public void rotateRight(RedBlackTree Node) {
+        rbtNode.setChildRight(rightChild.getChildLeft());
+        if(rightChild.getChildLeft() != null) {
+            rightChild.getChildLeft().setParent(rbtNode);
+        }
+        rightChild.setChildLeft(rightChild);
+        rbtNode.setParent(rightChild);
 
+        replaceParentsChild(parent,rbtNode,rightChild);
     }
+
+    public void rotateRight(RedBlackTree rbtNode) {
+        RedBlackTree parent = rbtNode.getParent();
+        RedBlackTree leftChild = rbtNode.getChildLeft();
+
+        rbtNode.setChildLeft(leftChild.getChildRight());
+        if(leftChild.getChildRight() != null) {
+            leftChild.getChildRight().setParent(rbtNode);
+        }
+        leftChild.setChildRight(rbtNode);
+        rbtNode.setParent(leftChild);
+
+        replaceParentsChild(parent,rbtNode,leftChild);
+    }
+
+    public void replaceParentsChild(RedBlackTree parent, RedBlackTree oldChild, RedBlackTree newChild) {
+        if(java.util.Objects.equals(parent,null)) root = newChild;
+        else if (java.util.Objects.equals(parent.getChildLeft(), oldChild)) parent.setChildLeft(newChild);
+        else if (java.util.Objects.equals(parent.getChildRight(),parent)) parent.setChildRight(newChild);
+        else throw new IllegalStateException("Node is not a child of its parent");
+
+        if(newChild != null) newChild.setParent(parent);
+    }
+
     //operations on red-black tree
-    public RedBlackTree searchNode(RedBlackTree Node) {
-        return Node;
-    }
-    public RedBlackTree insertNode(RedBlackTree Node) {
-        return Node;
-    }
-    public void deleteNode(RedBlackTree Node) {
-
+    public RedBlackTree insertNode() {
+        return null;
     }
 
     public String toString() {
@@ -32,23 +65,20 @@ public class RedBlackTree {
         //output function in DOT format.
         return "";
     }
-    /**
-     * @return BLACK: "true" or RED: "false"
-     */
 
+    public RedBlackTree getChildLeft() {return childLeft;}
+    public void setChildLeft(RedBlackTree childLeft) {this.childLeft = childLeft;}
+    public RedBlackTree getChildRight() {return childRight;}
 
-
-    public boolean getColor() {
-        return color;
-    }
+    public void setChildRight(RedBlackTree childRight) {this.childRight = childRight;}
+    public RedBlackTree getParent() {return parent;}
+    public void setParent(RedBlackTree parent) {this.parent = parent;}
+    public Integer getValue() {return value;}
+    public void setValue(Integer value) {this.value = value;}
+    public boolean getColor() {return color;}
     public String getColorToString() {
         if(this.getColor()) return "black";
         return "red";
     }
-    /**
-     * @param color BLACK: "true" or RED: "false"
-     */
-    public void setColor(boolean color) {
-        this.color = color;
-    }
+    public void setColor(boolean color) {this.color = color;}
 }
