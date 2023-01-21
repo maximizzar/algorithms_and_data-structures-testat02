c8948d1b2530b1f220c4f4ea15c2ff012b26da0f
Lucas <94450077+LGreubel@users.noreply.github.com>
2023-01-21 20:05:31 +0100

    red black tree logic fix 1 issue all nodes except root are red

--- src/RedBlackTree.java
+++ src/RedBlackTree.java
@@ -1,6 +1,65 @@
 /**
  * a red-black tree implementation by the unavailable team!
  */
+class Node {
+    private Node parent, childLeft, childRight;
+    protected Node root;
+    private Integer value;
+    private boolean color;
+
+    public Node(Integer value) {
+        setValue(value);
+    }
+
+    public Node getChildLeft() {
+        return childLeft;
+    }
+
+    public Node getChildRight() {
+        return childRight;
+    }
+
+    public Node getParent() {
+        return parent;
+    }
+
+    public Integer getValue() {
+        return value;
+    }
+
+    public boolean isColorBlack() {
+        return color;
+    }
+
+    public boolean isColorRed() {
+        return !color;
+    }
+
+    public void setChildLeft(Node childLeft) {
+        this.childLeft = childLeft;
+    }
+
+    public void setChildRight(Node childRight) {
+        this.childRight = childRight;
+    }
+
+    public void setParent(Node parent) {
+        this.parent = parent;
+    }
+
+    public void setValue(Integer value) {
+        this.value = value;
+    }
+
+    public void setColorBlack() {
+        this.color = true;
+    }
+
+    public void setColorRed() {
+        this.color = false;
+    }
+}
+
 public class RedBlackTree extends Node {
 
     public RedBlackTree(Integer value) {
@@ -36,13 +95,13 @@ public class RedBlackTree extends Node {
         Node rightChild = rbtNode.getChildRight();
 
         rbtNode.setChildRight(rightChild.getChildLeft());
-        if(rightChild.getChildLeft() != null) {
+        if (rightChild.getChildLeft() != null) {
             rightChild.getChildLeft().setParent(rbtNode);
         }
         rightChild.setChildLeft(rightChild);
         rbtNode.setParent(rightChild);
 
-        replaceParentsChild(parent,rbtNode,rightChild);
+        replaceParentsChild(parent, rbtNode, rightChild);
     }
 
     public void rotateRight(Node rbtNode) {
@@ -50,19 +109,19 @@ public class RedBlackTree extends Node {
         Node leftChild = rbtNode.getChildLeft();
 
         rbtNode.setChildLeft(leftChild.getChildRight());
-        if(leftChild.getChildRight() != null) {
+        if (leftChild.getChildRight() != null) {
             leftChild.getChildRight().setParent(rbtNode);
         }
         leftChild.setChildRight(rbtNode);
         rbtNode.setParent(leftChild);
 
-        replaceParentsChild(parent,rbtNode,leftChild);
+        replaceParentsChild(parent, rbtNode, leftChild);
     }
 
     public void replaceParentsChild(Node parent, Node oldChild, Node newChild) {
-        if(java.util.Objects.equals(parent,null)) root = newChild;
+        if (java.util.Objects.equals(parent, null)) root = newChild;
         else if (java.util.Objects.equals(parent.getChildLeft(), oldChild)) parent.setChildLeft(newChild);
-        else if (java.util.Objects.equals(parent.getChildRight(),parent)) parent.setChildRight(newChild);
+        else if (java.util.Objects.equals(parent.getChildRight(), parent)) parent.setChildRight(newChild);
         else throw new IllegalStateException("Node is not a child of its parent");
 
         if (newChild != null) newChild.setParent(parent);
@@ -85,91 +144,65 @@ public class RedBlackTree extends Node {
             parent.setColorBlack();
             return;
         }
+        Node RtheFruitDude;
+        Node LtheFruitDude;
         //theFruitDude either nil or black
-        Node theFruitDude = getTheFruitDude(parent);
+        if (grandparent.getChildLeft() == parent.getParent()) {
+            RtheFruitDude = grandparent.getChildRight();
+            if (grandparent.getChildRight() == parent.getParent()) {
+                LtheFruitDude = grandparent.getChildLeft();
 
-        //Uncle is red
-        if (theFruitDude != null && theFruitDude.isColorRed()) {
-            parent.setColorBlack();
-            grandparent.setColorRed();
-            theFruitDude.setColorBlack();
 
-            //recoloring grandparent may break RBTProperties
-            fixRBTPropertiesInsert(grandparent);
+                //Right-Uncle is red
+                if (RtheFruitDude != null && RtheFruitDude.isColorRed()) {
+                    parent.setColorBlack();
+                    grandparent.setColorRed();
+                    RtheFruitDude.setColorBlack();
 
-        } else if (parent == grandparent.getChildLeft()) { //Parent is left child of grandparent
-            if (rbtNode == parent.getChildRight()) {
-                rotateLeft(parent);
-                parent = rbtNode;
+                    //recoloring grandparent may break RBTProperties
+                    fixRBTPropertiesInsert(grandparent);
+
+                }
+                //Left-Uncle is red
+                if (LtheFruitDude != null && LtheFruitDude.isColorRed()) {
+                    parent.setColorBlack();
+                    grandparent.setColorRed();
+                    LtheFruitDude.setColorBlack();
+
+                    //recoloring grandparent may break RBTProperties
+                    fixRBTPropertiesInsert(grandparent);
+
+                } else if (parent == grandparent.getChildLeft()) { //Parent is left child of grandparent
+                    if (rbtNode == parent.getChildRight()) {
+                        rotateLeft(parent);
+                        parent = rbtNode;
+                    }
+                    rotateRight(grandparent);
+
+                    //the former parent and grandparent
+                    parent.setColorBlack();
+                    grandparent.setColorRed();
+
+                } else { //Parent is right child of grandparent
+                    if (rbtNode == parent.getChildLeft()) {
+                        rotateRight(parent);
+
+                        parent = rbtNode;
+                    }
+
+                    rotateLeft(grandparent);
+
+                    //the former parent and grandparent
+                    parent.setColorBlack();
+                    grandparent.setColorRed();
+                }
+
             }
-            rotateRight(grandparent);
-
-            //the former parent and grandparent
-            parent.setColorBlack();
-            grandparent.setColorRed();
-
-        } else { //Parent is right child of grandparent
-            if (rbtNode == parent.getChildLeft()) {
-                rotateRight(parent);
-
-                parent = rbtNode;
-            }
-
-            rotateLeft(grandparent);
-
-            //the former parent and grandparent
-            parent.setColorBlack();
-            grandparent.setColorRed();
         }
     }
+                public String toDot() {
+                return null;
+                }
 
-    private Node getTheFruitDude(Node parent) {
-        Node grandparent = parent.getParent().getParent();
-        if(grandparent.getChildLeft() == parent.getParent()) return grandparent.getChildRight();
-        else if (grandparent.getChildRight() == parent.getParent()) return grandparent.getChildLeft();
-        throw new IllegalStateException("Parent is not a child of its grandparent");
-    }
 
-    public String toString() {
-        //output function as Simple String.
-        return "";
-    }
-    /**
-     * @return a string which is PowerPoint DOT compatible.
-     */
-    public String toDOT() {
-        //output function in DOT format.
-        return "";
-    }
-    public String rbtNodeToDot(Node rbtNode) {
-        //this-value
-        //parent-value
-        //childLeft-value
-        //childRight-value
-        return "";
-    }
-}
-
-class Node {
-    private Node parent, childLeft, childRight;
-    protected Node root;
-    private Integer value;
-    private boolean color;
-
-    public Node (Integer value) {
-        setValue(value);
-    }
-
-    public Node getChildLeft() {return childLeft;}
-    public Node getChildRight() {return childRight;}
-    public Node getParent() {return parent;}
-    public Integer getValue() {return value;}
-    public boolean isColorBlack() {return color;}
-    public boolean isColorRed() {return !color;}
-    public void setChildLeft(Node childLeft) {this.childLeft = childLeft;}
-    public void setChildRight(Node childRight) {this.childRight = childRight;}
-    public void setParent(Node parent) {this.parent = parent;}
-    public void setValue(Integer value) {this.value = value;}
-    public void setColorBlack() {this.color = true;}
-    public void setColorRed() {this.color = false;}
 }
