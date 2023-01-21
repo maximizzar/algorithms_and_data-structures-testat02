f0e91bc5b96dbd4f0d432f6cd510467fccd95d41
maximizzar <maximizzar@web.de>
2023-01-21 22:32:31 +0100

    every diagramm in own .dot file

--- src/Main.java
+++ src/Main.java
@@ -2,67 +2,61 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Random;
 import java.util.BitSet;
-import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        FileWriter fileWriter;
         Integer elementCount,seed,maxRandomNumber;
-
-        Boolean test = true;
-
-        if(test) {
             elementCount = 15;
             seed = 1337;
             maxRandomNumber = 100;
 
-        } else {
-            elementCount = scanner.nextInt();
-            seed = scanner.nextInt();
-            maxRandomNumber = scanner.nextInt();
-        }
         try {
-            FileWriter fileWriter = new FileWriter("src/RedBlackTree.dot");
-            fileWriter.write(outputRedBlackTreeWithRandomElements(elementCount,seed,maxRandomNumber));
-            fileWriter.close();
+            outputRBTWithRandomElements(elementCount,seed,maxRandomNumber);
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
     }
-    public static String outputRedBlackTreeWithRandomElements(Integer elementCount,
-                                                             Integer seed,
-                                                             Integer maxRandomNumber) {
-
+    public static void outputRBTWithRandomElements(Integer elementCount, Integer seed, Integer maxRandomNumber)
+            throws IOException {
         BitSet alreadySeen = new BitSet(maxRandomNumber);
         Random random = new Random(seed);
-        StringBuilder stringBuilder = new StringBuilder();
+        StringBuilder stringBuilder;
+        FileWriter fileWriter;
 
         Integer value = random.nextInt(maxRandomNumber) + 1;
         RedBlackTree tree = new RedBlackTree(value);
 
-        stringBuilder.append("//RedBack_")
-                .append(elementCount)
-                .append("_elements")
-                .append("\n");
-
         for (int i = 0; i < elementCount;) {
             value = random.nextInt(maxRandomNumber);
 
-            if (!alreadySeen.get(value)) {
-                alreadySeen.set(value); i++;
-                tree.insertNode(value);
+            if (alreadySeen.get(value)) continue;
 
-                stringBuilder.append("// ")
-                        .append(" ".repeat(16))
-                        .append(" round: ").append(i).append(" "). append("\n\n");
+            stringBuilder = new StringBuilder();
+            String path = "rbt/" + i + ".dot";
+            fileWriter = new FileWriter(path);
 
-                stringBuilder.append("digraph RBTree {").append("\n");
-                tree.toDOT(tree.root,stringBuilder);
-                stringBuilder.append("\tnil [style = filled, fillcolor = black, fontcolor = white];")
-                        .append("\n")
-                        .append("}").append("\n\n");
-            }
+            alreadySeen.set(value); i++;
+            tree.insertNode(value);
+
+            roundHeadingComment(stringBuilder, i);
+            currentDotDiagram(stringBuilder, tree);
+
+            fileWriter.write(stringBuilder.toString());
+            fileWriter.close();
         }
-        return stringBuilder.toString();
+    }
+    private static void currentDotDiagram(StringBuilder stringBuilder, RedBlackTree tree) {
+        stringBuilder.append("digraph RBTree {").append("\n");
+        tree.toDOT(tree.root, stringBuilder);
+        stringBuilder.append("\tnil [style = filled, fillcolor = black, fontcolor = white];")
+                .append("\n")
+                .append("}").append("\n\n");
+    }
+    private static void roundHeadingComment(StringBuilder stringBuilder, int i) {
+        stringBuilder.append("// ")
+                .append(" ".repeat(16))
+                .append(" round: ").append(i)
+                .append(" "). append("\n\n");
     }
 }
